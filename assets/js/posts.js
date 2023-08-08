6df0dbe310e00b705e9871f303f84fd0f6e06210
Pablo Couto <pablocouto@aluno.senai.br>
2023-08-08 15:14:38 -0300

    feat: create showPost function

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -10,8 +10,10 @@ function savePost() {
     if(title && category && resume && publisher && date) {
         storePost(title, category, resume, publisher, date)
         cleanFields()
+        showPosts()
     } else {
         alert("Preencha todos os campos!")
+        cleanFields()
     }
 }
 
@@ -33,6 +35,25 @@ function storePost(title, category, resume, publisher, date) {
     }
 
     posts.push(post)
-
     console.log(posts)
+}
+
+function showPosts() {
+    let showContent = "";
+
+    posts.forEach((post, index) => {
+        showContent += `
+            <div class="itemPost">
+                <h2>${post.title}</h2>
+                    <p><strong>Categoria: </strong>${post.category}</p>
+                    <p><strong>Resumo: </strong>${post.resume}</p>
+                    <p><strong>Autor: </strong>${post.publisher}</p>
+                    <p><strong>Data de publicação: </strong>${post.date}</p>
+
+                <button onclick="editPost(${index})">Editar</button>
+                <button onclick="deletePost(${index})">Excluir</button>
+            </div>`;
+    })
+
+    document.getElementById("list").innerHTML = showContent;
 }
